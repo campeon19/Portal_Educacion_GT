696b54654829a526b565977357d0724365a7bbf7
José Hurtarte <jjhh121106@gmail.com>
2021-10-23 00:23:25 -0600

    Course cards

--- src/Components/Courses/CourseCard/Course.jsx
+++ src/Components/Courses/CourseCard/Course.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Course.css';
-import calculus from './assets/calculus.jpg'
-function Course() {
+import calculus from '../assets/calculus.jpg'
+function Course(props) {
     return (
         <div className ='course-card'>
-            <div class="course-card container h-100 full-width">
+            <div class="container h-100 full-width">
     <div class="row align-items-center">
         <div class="col mx-auto text-center">
-                <h3>Calculo 11</h3>
+                <h3>{props.course.nombre}</h3>
             
         </div>
     </div>
     <div class="row align-items-center">
         <div class="col mx-auto">
-                <img className="inner-card-image " src={calculus}/>
+                <img className="inner-card-image " src={props.course.picture}/>
             
         </div>
     </div>
     <div class="row align-items-center ">
         <div class="col mx-auto lead text-center">
-                Limites integrales y derivadas, ademas de fundamentos de aplicaciones y optimizacion
+                {props.course.brief}
             
         </div>
     </div>
---
+++ src/Components/Courses/CourseContainer.jsx
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import Course from './CourseCard/Course';
+import './CourseContainer.css'
+
+import math from './assets/calculus.jpg'
+import physics from './assets/physics.jpg'
+import language from './assets/communication.jpg'
+import chemistry from './assets/chemistry.jfif'
+import biology from './assets/biology.jpg'
+
+const cursos = [
+    {id:0, tema:'matemática', nombre: 'Calculo 1', brief: 'Aprende limites, derivadas e integrales. Además de optimización de funciones', picture:math},
+    {id:1, tema:'matemática', nombre: 'Precalculo', brief: 'Aprende reglas de exponentes, logaritmicas y a resolver ecuaciones trigonometricas', picture:math},
+    {id:2, tema:'física', nombre: 'Fisica fundamental', brief: 'Aprende acerca de los vectores, que significan y como aplicarlos', picture:physics},
+    {id:3, tema:'lenguaje', nombre: 'Comunicacion', brief: 'Aprende redacción, ortografía, gramática y a comunicarte efectivamente', picture:language},
+    {id:4, tema:'química', nombre: 'Química general', brief: 'Aprende acerca reacciones básicas, el átomo y estequeometría', picture:chemistry},
+    {id:5, tema:'biologia', nombre: 'Biología básica', brief: 'Aprende acerca de la celula, microorganismos y la fotosintesis', picture:biology}
+]
+function CourseContainer() {
+    const [courses, setCourses] = useState(cursos);
+
+    return (
+        <div className='courses-countainer'>
+            {
+                courses.map((course)=>{
+                    return <Course course={course}/>
+                })
+            }
+        </div>
+    )
+}
+
+export default CourseContainer

86e4a3ba0d3629719da676b22c6f13677fa9eb63
Christian Pérez <christiandp@live.com>
2021-10-23 01:15:09 -0600

    Cambios en la pestaña beca

--- src/Components/Becas Screen/BecasScreen.jsx
+++ src/Components/Becas Screen/BecasScreen.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import BecasCard from '../Becas/Becas'
+import imgHeader from '../Becas/Assets/becas_header.jpg'
+import './BecasScreen.css'
 
 export default function Becas() {
     return (
         <div>
+            <div className='container-size'>
+                <img className='img-header' src={imgHeader} />
+            </div>
             <div>
                 <BecasCard />
                 <BecasCard />

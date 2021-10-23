9bf9d495efd95e627f1ed57cf99c5dbd552caf57
José Hurtarte <jjhh121106@gmail.com>
2021-10-22 22:26:39 -0600

    Course card

--- src/Components/Courses/Course.jsx
+++ src/Components/Courses/Course.jsx
@@ -1,8 +1,32 @@
-
-export default function Course() {
+import React from 'react';
+import './Course.css';
+import calculus from './assets/calculus.jpg'
+function Course() {
     return (
-        <div>
+        <div className ='course-card'>
+            <div class="course-card container h-100 full-width">
+    <div class="row align-items-center">
+        <div class="col mx-auto text-center">
+                <h3>Calculo 11</h3>
             
+        </div>
+    </div>
+    <div class="row align-items-center">
+        <div class="col mx-auto">
+                <img className="inner-card-image " src={calculus}/>
+            
+        </div>
+    </div>
+    <div class="row align-items-center ">
+        <div class="col mx-auto lead text-center">
+                Limites integrales y derivadas, ademas de fundamentos de aplicaciones y optimizacion
+            
+        </div>
+    </div>
+</div>
         </div>
     )
 }
+
+export default Course
+

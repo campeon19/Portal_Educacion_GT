687ab6918124c8414b43a9205b4e51dae2ffa278
Christian Pérez <christiandp@live.com>
2021-10-24 12:59:01 -0600

    Navbar icons and links implemented

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import logo from './Assets/logo.png';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faBook, faBriefcase, faHome, faInfo } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
@@ -24,16 +27,16 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navcol-1">
               <ul className="navbar-nav flex-grow-1 justify-content-center">
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link px-5" aria-current="page" href="#">Home</a>
+                    <Link to='/' className="nav-link px-5" aria-current="page" href="#"><FontAwesomeIcon icon={faHome}></FontAwesomeIcon> Home</Link>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link px-5" aria-current="page" href="#">Cursos</a>
+                    <Link to='/courses' className="nav-link px-5" aria-current="page" href="#"><FontAwesomeIcon icon={faBook}></FontAwesomeIcon> Cursos</Link>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link px-5" aria-current="page" href="#">Informacion Becas</a>
+                    <Link to='/becas' className="nav-link px-5" aria-current="page" href="#"><FontAwesomeIcon icon={faInfo}></FontAwesomeIcon> Informacion Becas</Link>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className="nav-link px-5" aria-current="page" href="#">Informacion Trabajos</a>                  
+                    <Link to='/jobs' className="nav-link px-5" aria-current="page" href="#"><FontAwesomeIcon icon={faBriefcase}></FontAwesomeIcon> Informacion Trabajos</Link>                  
                 </li>
               </ul>
             </div>
